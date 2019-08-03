54c38a7207ec28583f80a8e7f4884c1bbb132eb9
Vicfred <vicfredboy@gmail.com>
2019-08-03 01:10:46 -0500

    remove sdl2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <cstdio>
-#include <SDL2/SDL.h>
 #include "chip8.h"
 
 chip8 momoko;
@@ -16,25 +15,5 @@ int main(int argc, char **argv) {
     //momoko.debugMemory();
     //momoko.debugStack();
 
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *win = SDL_CreateWindow("CHIP-8 Emulator",
-                                        SDL_WINDOWPOS_CENTERED,
-                                        SDL_WINDOWPOS_CENTERED,
-                                        1280, 640,
-                                        SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
-    );
-
-    printf("SDL Error: %s\n", SDL_GetError());
-
-    SDL_Renderer *rnd = SDL_CreateRenderer(win, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(rnd, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(rnd);
-    SDL_RenderPresent(rnd);
-
-    SDL_Delay(10000);
-
-    SDL_DestroyRenderer(rnd);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
     return 0;
 }
